e617842b7677905a4d7cfff65ec885cf09e57955
Cleidson <cleidson.ramosmartins@gmail.com>
2021-05-18 21:07:55 -0300

    adicionado comentarios

--- digitalcep/exemplocep.php
+++ digitalcep/exemplocep.php
@@ -1,10 +1,14 @@
 <?php
 
+//chama no composer
 require_once "vendor/autoload.php";
 
 use cleidson\digitalcep\Search;
 
+//cria um objeto que é uma instancia da classe
 $busca = new Search;
 
 $resultado = $busca->getEnderecoCEP('74766030');
+
+//print_r exibe informação sobre uma variável em uma maneira que é legível para humanos.
 print_r($resultado);
--- digitalcep/src/Search.php
+++ digitalcep/src/Search.php
@@ -8,11 +8,13 @@ class Search
 
     public function getEnderecoCEP(string $cep) : array
     {
-        //verifica se todos são numeros
+        //verifica se todos são numeros e se não for vai substituir por nada ''
         $cep = preg_replace('/[^0-9]/im','', $cep);
         
+        // pega o conteudo da pagina https://viacep.com.br/ws/74766030/json
         $get = file_get_contents($this->url . $cep ."/json");
         
+        // retora um json do conteudo
         return (array) json_decode($get);
     }
 }
