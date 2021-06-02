07d7a85d28934db7003b3b650958a02d1fd892ef
Cleidson <63806835+CleidsonRamos@users.noreply.github.com>
2021-06-02 12:32:31 -0300

    Update formvendedor.php

--- pdo/view/formvendedor.php
+++ pdo/view/formvendedor.php
@@ -6,7 +6,7 @@
 </button>
 
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Cadastro de Vendedores</h5>
@@ -15,47 +15,45 @@
         </button>
       </div>
       <div class="modal-body">
-          <form>
+          <form action='inserirvendedor.php' method='post'>
             <div class="form-row">
               <div class="form-group col-md-6">
                 <label for="inputEmail4">Nome</label>
-                <input type="id" class="form-control" id="inputEmail4" placeholder="nome">
+                <input name="nome" type="id" class="form-control" id="inputEmail4" placeholder="">
               </div>
               <div class="form-group col-md-6">
                 <label for="inputPassword4">Email</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="Email">
+                <input name="email" type="text" class="form-control" id="inputPassword4" placeholder="">
               </div>
             </div>
             <div class="form-row">
               <div class="form-group col-md-6">
                 <label for="inputEmail4">Empresa</label>
-                <input type="id" class="form-control" id="inputEmail4" placeholder="Empresa">
+                <input name="id_empresa" type="id" class="form-control" id="inputEmail4" placeholder="">
               </div>
               <div class="form-group col-md-6">
                 <label for="inputPassword4">Unidade</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="Unidade">
+                <input name="id_unidade" type="text" class="form-control" id="inputPassword4" placeholder="">
               </div>
             </div>
-
             <div class="form-row">
               <div class="form-group col-md-6">
                 <label for="inputEmail4">Centro de Resultado</label>
-                <input type="id" class="form-control" id="inputEmail4" placeholder="Centro de Resultado">
+                <input name="id_centro_de_resultado" type="id" class="form-control" id="inputEmail4" placeholder="">
               </div>
               <div class="form-group col-md-6">
                 <label for="inputPassword4">Usuario</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="Usuario">
+                <input name="id_usuario" type="text" class="form-control" id="inputPassword4" placeholder="">
               </div>
               <div class="form-group col-md-6">
                 <label for="inputPassword4">Local de Estoque</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="Local de Estoque">
+                <input name="id_local_estoque" type="text" class="form-control" id="inputPassword4" placeholder="">
               </div>
             </div>
-
             <div class="form-group">
               <div class="form-check">
                 <label class="form-check-label">
-                  <input class="form-check-input" type="checkbox" value="" checked>
+                  <input name="status" class="form-check-input" type="checkbox" value="1" checked>
                   Ativado
                   <span class="form-check-sign">
                     <span class="check"></span>
@@ -64,7 +62,7 @@
               </div>
             </div>
           </div>
-           <div class="modal-footer">
+      <div class="modal-footer">
         <button type="button" class="btn btn-secondary btn-round" data-dismiss="modal">
           Fechar  &nbsp
           <span class="material-icons">
@@ -77,7 +75,7 @@
           save
         </span>
         </button>
-     </form>
+       </form>
       </div>
     </div>
   </div>
