a579bb2e7ccf9b6eefb3ec48d82e4f224b4b30f0
Cleidson <63806835+CleidsonRamos@users.noreply.github.com>
2021-06-02 11:23:34 -0300

    Incluindo o dashboard do Google DataStudio

---
+++ pdo/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+    include 'view/head.php';
+    include 'view/sidebar.php';
+?>
+
+<div class="content">
+<div class="container-fluid">
+  <iframe width="980" height="550" src="https://datastudio.google.com/embed/reporting/ca9b2c38-78a9-47ee-9dae-40da01f52e33/page/Jof2B" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+</div>
+
+<?php
+    include 'view/footer.php';
+?>
