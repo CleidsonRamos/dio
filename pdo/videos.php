4dd58ce50c9cd94c97a4711ac380601e86490c21
Cleidson <63806835+CleidsonRamos@users.noreply.github.com>
2021-06-02 11:27:32 -0300

    Pagina com vídeos

---
+++ pdo/videos.php
@@ -0,0 +1,84 @@
+<?php
+    include 'view/head.php';
+    include 'view/sidebar.php';
+?>
+
+<div class="content">
+<div class="container-fluid">
+
+<div class="row"> <!--Linha 1-->
+<div class="card" style="width: 20rem; margin: 10px">
+<iframe class="card-img-top" src="https://www.youtube.com/embed/vsrDLH7BNYo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="card-body">
+    <p class="card-text">Passo a passo para a emissão do cerificado Bird ID</p>
+  </div>
+</div>
+
+<div class="card" style="width: 20rem; margin: 10px">
+<iframe class="card-img-top" src="https://www.youtube.com/embed/0hKiS74CP9E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="card-body">
+    <p class="card-text">Passo a passo para fazer o agendamento de vídeo conferência.</p>
+  </div>
+</div>
+
+<div class="card" style="width: 20rem; margin: 10px">
+<iframe class="card-img-top" src="https://www.youtube.com/embed/VT-s1MaAWg0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="card-body">
+    <p class="card-text">Passo a passo para baixar o cerificado A1.</p>
+  </div>
+</div>
+</div> <!--Fim da Linha 1-->
+
+<div class="row"> <!--Linha 2-->
+<div class="card" style="width: 20rem; margin: 10px">
+<iframe class="card-img-top" src="https://www.youtube.com/embed/XgQh6s2XWco" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="card-body">
+    <p class="card-text">Passo a passo para baixar o cerificado A3.</p>
+  </div>
+</div>
+
+<div class="card" style="width: 20rem; margin: 10px">
+<iframe class="card-img-top" src="https://www.youtube.com/embed/BXMhtKlmObU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="card-body">
+    <p class="card-text">Passo a passo para sincronização do Bird ID no desktop.</p>
+  </div>
+</div>
+
+<div class="card" style="width: 20rem; margin: 10px">
+<iframe class="card-img-top" src="https://www.youtube.com/embed/K-d9KSZ0nXw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="card-body">
+    <p class="card-text">Como redefinir a senha do bird id.</p>
+  </div>
+</div>
+</div> <!--Fim da Linha 2-->
+
+
+<div class="row"> <!--Linha 3-->
+<div class="card" style="width: 20rem; margin: 10px">
+<iframe class="card-img-top" src="https://www.youtube.com/embed/Is-jJNr3eKY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="card-body">
+    <p class="card-text">Passo a passo para instalar Bird ID no desktop.</p>
+  </div>
+</div>
+
+<div class="card" style="width: 20rem; margin: 10px">
+<iframe class="card-img-top" src="https://www.youtube.com/embed/GezTYedEMvY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="card-body">
+    <p class="card-text">Passo a passo para fazer o agendamento com a CNH.</p>
+  </div>
+</div>
+
+<div class="card" style="width: 20rem; margin: 10px">
+<iframe class="card-img-top" src="https://www.youtube.com/embed/0hKiS74CP9E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="card-body">
+    <p class="card-text">Passo a passo para fazer atendimento por video conferencia.</p>
+  </div>
+</div>
+</div> <!--Fim da Linha 3-->
+
+</div>
+</div>
+
+<?php
+    include 'view/footer.php';
+?>
