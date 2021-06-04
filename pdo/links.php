1d3315e877514ace0bb39e7eebca2f07ac993b51
Cleidson <63806835+CleidsonRamos@users.noreply.github.com>
2021-06-04 13:48:07 -0300

    Pagina de links uteis

---
+++ pdo/links.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+include 'view/head.php';
+include 'view/sidebar.php';
+
+?>
+ 
+<div class="content">
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header card-header-primary">
+          <h4 class="card-title">Links Uteis</h4>
+          <p class="card-category">Links para consulta de documentos</p>
+        </div>
+        <div class="card-body" style="height: 1650px; width: 100%; display: block;">
+          <iframe width="100%" height="100%" frameBorder="0" src="https://docs.google.com/document/d/e/2PACX-1vSQlD1_b4qF-YNwt7sLpmiOKuko9KNtAp-gY6vhc7w5S019D3bhIfYjQPR_pHnH-ZmJBO1oJvhuruIC/pub?embedded=true"></iframe>
+        </div>
+        </div>
+      </div>
+    </div>   
+  </div>
+</div>
+</div>
+
+<?php
+    include 'view/footer.php';
+?>
